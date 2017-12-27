8dcb88315432ace495d00e2ab186bef5aa8d0f67
Eric Puigmarti <eric.puigmarti@gmail.com>
2017-12-27 14:18:05 -0500

    styling changes :sparkles:

--- js/main.js
+++ js/main.js
@@ -4,3 +4,10 @@ $('.actions').on('click', function(e){
 		$(this).text( $(this).text() == "Show more" ? "Show Less" : "Show more" );
 		// $(this).closest('article.post').find('.hidden-content').slideToggle()
 	})
+
+// Vue.js filters
+// ---- ADDED BACK TO INDEX.HTML
+
+// Vue.filter('snippet', function(value){
+// 	return value.slice(0,100);
+// });
